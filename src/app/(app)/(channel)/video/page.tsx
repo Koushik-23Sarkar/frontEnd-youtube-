26f1cf29efe3f05884f22b8e7878b4a832b6a355
KOUSHIK SARKAR <koushiksarkar7102@gmail.com>
2025-10-09 15:59:06 +0530

    Basic frontEnd bluePrint

---
+++ src/app/(app)/(channel)/video/page.tsx
@@ -0,0 +1,8 @@
+import ChannelEmptyVideoPage from "@/app/components/channelEmptyVideoPage";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <ChannelEmptyVideoPage/>
+  );
+}
