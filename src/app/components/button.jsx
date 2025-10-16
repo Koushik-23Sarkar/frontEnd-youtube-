62b8e9be572f77e448f5ecd422722b786307f37b
KOUSHIK SARKAR <koushiksarkar7102@gmail.com>
2025-10-16 13:17:49 +0530

    add /login /register with <login> <register> button

--- src/app/components/button.jsx
+++ src/app/components/button.jsx
@@ -1,6 +1,20 @@
+"use Client"
+
+import { useRouter } from "next/navigation";
+
 export default function Button({ btnColor,btnText, textColor}) {
+  const router = useRouter();
+  const handleClick = (btnText)=>{
+    if(btnText=="Log in"){
+      router.push("/login");
+    }
+    if(btnText=="Sign up"){
+      router.push("/register")
+    }
+  }
+
   return (
-    <button className={`mr-1 w-full bg-[#${btnColor}] px-3 py-2 text-center font-bold text-${textColor} shadow-[5px_5px_0px_0px_#4f4e4e] transition-all duration-150 ease-in-out active:translate-x-[5px] active:translate-y-[5px] active:shadow-[0px_0px_0px_0px_#4f4e4e] sm:w-auto`}>
+    <button onClick={()=>handleClick(btnText)} className={`mr-1 w-full bg-[#${btnColor}] px-3 py-2 text-center font-bold text-${textColor} shadow-[5px_5px_0px_0px_#4f4e4e] transition-all duration-150 ease-in-out active:translate-x-[5px] active:translate-y-[5px] active:shadow-[0px_0px_0px_0px_#4f4e4e] sm:w-auto`}>
       {btnText}
     </button>
   );
